990450554f62c8effc6759eac840a6ec287ed190
Yoocee Ansah <yooceeansah@gmail.com>
2024-07-27 18:29:37 +0100

    added group chat functionalities

--- a_rtchat/forms.py
+++ a_rtchat/forms.py
@@ -7,6 +7,33 @@ class ChatMessageCreateForm(ModelForm):
         model = GroupMessage
         fields = ['body']
         widgets = {
-            'body' : forms.TextInput(
-                attrs={'placeholder': 'Add message ...', 'class': 'p-4 text-black', 'maxlength' : '300', 'autofocus': True }),
+            'body' : forms.TextInput(attrs={
+                'placeholder': 'Add message ...', 
+                'class': 'p-4 text-black', 
+                'maxlength' : '300',
+                'autofocus': True,
+            })
+        }
+class NewGroupForm(ModelForm):
+    class Meta:
+        model = ChatGroup
+        fields = ['groupchat_name']
+        widgets = {
+            'groupchat_name': forms.TextInput(attrs={
+                'placeholder': 'Add message ...', 
+                'class': 'p-4 text-black', 
+                'maxlength' : '300',
+                'autofocus': True,
+            })
+        }
+
+class ChatRoomEditForm(ModelForm):
+    class Meta:
+        model = ChatGroup
+        fields = ['groupchat_name']
+        widgets = {
+            'groupchat_name': forms.TextInput(attrs={ 
+                'class': 'p-4 text-xl font-bold mb-4', 
+                'maxlength' : '300',
+            })
         }
